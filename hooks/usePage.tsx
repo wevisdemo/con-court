a1f7a8ff12b48bc1c7e3ca8aa300359269df065d
Jame <>
2023-12-08 03:10:06 +0700

    fix go to section function

--- hooks/usePage.tsx
+++ hooks/usePage.tsx
@@ -1,16 +1,27 @@
 import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export const usePage = () => {
   const router = useRouter();
   const pathname = usePathname();
+  let interval: NodeJS.Timeout;
 
   const goToSection = (path: string, id: string) => {
     if (path !== pathname) router.push(path);
-    setTimeout(() => {
+    interval = setInterval(() => {
       const element = document.getElementById(id);
-      if (element) element.scrollIntoView();
-    }, 200);
+      if (element) {
+        element.scrollIntoView();
+        clearInterval(interval);
+      }
+    }, 1);
   };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return { goToSection };
 };
