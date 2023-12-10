ff05d37048a44fd3900120efeb289977d4be09bd
Jame <>
2023-12-10 13:39:15 +0700

    install million

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import million from 'million/compiler';
+
 /** @type {import('next').NextConfig} */
 
 const basePath = '/con-court';
@@ -12,4 +14,10 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const millionConfig = {
+  auto: true,
+  // if you're using RSC:
+  // auto: { rsc: true },
+};
+
+export default million.next(nextConfig, millionConfig);
